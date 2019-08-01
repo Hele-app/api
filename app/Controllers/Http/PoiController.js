ad3682d7ccd3381300a97e1170d8f371590b4ead
skendraoui <amina.skendraoui@epitech.eu>
2019-08-01 14:24:02 +0200

    POI delete edit update fait

--- app/Controllers/Http/PoiController.js
+++ app/Controllers/Http/PoiController.js
@@ -3,7 +3,6 @@
 const Poi= use('App/Models/Poi')
 const region= use('App/Models/Region')
 
-
 class PoiController {
 
     async home({response}) {
@@ -68,6 +67,38 @@ class PoiController {
         return response.redirect('/poi');
     }
 
+    async delete({ response, params }) {
+
+        let poi = await Poi.find(params.id);
+
+        await poi.delete();
+
+        return response.redirect('/poi');
+    }
+
+    async edit ({ params, response }) {
+        let poi = await Poi.find(params.id);
+        return response.json(poi)
+    }
+
+    async update ({ response, request, params}) {
+
+        let poi = await Poi.find(params.id);
+
+        poi.name = request.all().name
+        poi.adress = request.all().adress
+        poi.code_postal = request.all().code_postal
+        poi.phone = request.all().phone
+        poi.description = request.all().description
+        poi.horaire = request.all().horaire
+        poi.site = request.all().site
+        poi.latitude = request.all().latitude
+        poi.longitude = request.all().longitude
+
+        await poi.save();
+
+        return response.redirect('/poi');
+    }
 
 }
 
