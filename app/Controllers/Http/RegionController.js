2116124b0cf583d9d536bd5d08094ca9fce6c381
LeNiglo <lefrantguillaume@gmail.com>
2019-08-15 13:38:05 +0200

    fix region

--- app/Controllers/Http/RegionController.js
+++ app/Controllers/Http/RegionController.js
@@ -1,19 +1,21 @@
 'use strict'
-const Region= use('App/Models/Region')
+
+const Region = use('App/Models/Region')
+const Poi = use('App/Models/Poi')
 const Database = use('Database')
 
 class RegionController {
-    async home({response}) {
+    async all({response}) {
         const regions = await Region.all();
         return response.json(regions)
     }
 
-    async show({response, params, request}) {    
-        const query = await Database.from('pois')
+    async show({response, params, request}) {
+        const pois = await Poi.query()
         .where({region_id: [params.id] })
-        console.log(response.json(query))
-        return response.json(query)
-    }   
+        .fetch()
+        return response.json(pois)
+    }
 }
 
 module.exports = RegionController
