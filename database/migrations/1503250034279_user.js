569deb782626b15676cff5b44be7d90bd7c84a69
LeNiglo <lefrantguillaume@gmail.com>
2019-08-15 13:38:38 +0200

    fix user migration name

---
+++ database/migrations/1503250034279_user.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserSchema extends Schema {
+  up () {
+    this.create('users', (table) => {
+      table.increments()
+      table.string('phone', 20).notNullable().unique()
+      table.string('username', 100).notNullable().unique()
+      table.string('password', 100).notNullable()
+      table.string('email').index()
+      table.integer('birthyear').notNullable()
+      table.string('region', 100).notNullable()
+      table.enu('roles', ['YOUNG', 'MODERATOR', 'PROFESSIONAL', 'ADMIN']).defaultTo('YOUNG').index()
+      table.string('profession', 100)
+      table.string('city', 100)
+      table.string('phone_pro', 20)
+      table.bool('active').defaultTo(true).index()
+      table.datetime('last_login').defaultTo(this.fn.now())
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+}
+
+module.exports = UserSchema
+
