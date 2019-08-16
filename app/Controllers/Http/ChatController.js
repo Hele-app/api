fb2945e9d1f6117eb6b5432b80fd013832c8bca4
Alexandra <alexandra.nguyen@epitech.eu>
2019-08-16 10:38:19 +0200

    List of users y chat addded

--- app/Controllers/Http/ChatController.js
+++ app/Controllers/Http/ChatController.js
@@ -2,18 +2,33 @@
 
 const Chat = use('App/Models/Chat')
 
+
 class ChatController {
 
   async index({ response, auth }) {
 
     const user = await auth.getUser()
-    const chats = await user.chats().fetch()
+    
+    let chats = await user.chats().fetch()
+    chats = chats.toJSON()
 
-    if (chats.length !== 0) {
-      response.status(200).json(chats.toJSON())
-    } else {
-      response.status(404).send('Not found')
-    }
+  
+    if (chats.length === 0) { return response.status(404).send('Not found') }
+
+    const chatsID = chats.map(chat => {
+      return chat.id
+    })
+
+    let usersChats = await Chat
+      .query()
+      .whereIn('id', chatsID)
+      .with('users', builder => { builder.select('username') })
+      .fetch()
+
+    usersChats = usersChats.toJSON()
+
+    response.status(200).json(usersChats)
+
   }
 
   async show({ params: { id }, response }) {
