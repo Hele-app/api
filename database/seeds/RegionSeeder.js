e61a6f70c8381bbe9597bfdb8d248e0009becda3
miza92 <hakima.chapron@epitech.eu>
2019-08-26 17:01:03 +0200

    seed zoom region

--- database/seeds/RegionSeeder.js
+++ database/seeds/RegionSeeder.js
@@ -21,18 +21,18 @@ class RegionSeeder {
       {
         id: 1,
         name: "Auvergne-Rhône-Alpes",
-        latitude: "45.1916365",
-        longitude: "4.2190435",
-        latitudeDelta: "0.4306429923",
-        longitudeDelta: "2.5075569153"
+        latitude: "45.6196632385",
+        longitude: "4.3965625763",
+        latitudeDelta: "0.1610269994",
+        longitudeDelta: "0.4023970068"
       },
       {
         id: 2,
         name: "Bourgogne-Franche-Comté",
-        latitude: "47.545072",
-        longitude: "4.0678181648",
-        latitudeDelta: "0.0080100000",
-        longitudeDelta: "1.3786239624"
+        latitude: "47.4299964905",
+        longitude: "4.7077407837",
+        latitudeDelta: "0.5373160243",
+        longitudeDelta: "0.0917140022"
 
       },
       {
@@ -46,34 +46,34 @@ class RegionSeeder {
       {
         id: 4,
         name: "Centre-Val de Loire",
-        latitude: "47.644735",
-        longitude: "1.6135805",
-        latitudeDelta: "0.738306000000001",
-        longitudeDelta: "0.221621"
+        latitude: "47.8415870667",
+        longitude: "1.6135804653",
+        latitudeDelta: "0.3446039855",
+        longitudeDelta: "0.2216210067"
       },
       {
         id: 5,
         name: "Corse",
-        latitude: "42.1937145",
-        longitude: "9.332611",
-        latitudeDelta: "0.042304999999999",
-        longitudeDelta: "0.148555999999999"
+        latitude: "42.0571594238",
+        longitude: "9.2971897125",
+        latitudeDelta: "0.0126970001",
+        longitudeDelta: "0.2362619936"
       },
       {
         id: 6,
         name: "Grand Est",
-        latitude: "48.6778155",
-        longitude: "6.1132225",
-        latitudeDelta: "0.586437000000004",
-        longitudeDelta: "2.578813"
+        latitude: "48.8616676331",
+        longitude: "5.6731505394",
+        latitudeDelta: "0.2187290043",
+        longitudeDelta: "0.1024829969"
       },
       {
         id: 7,
         name: "Guadeloupe",
-        latitude: "16.2370375",
-        longitude: "-61.524765",
-        latitudeDelta: "0.194203000000002",
-        longitudeDelta: "0.120677999999998"
+        latitude: "16.2370376587",
+        longitude: "-61.5247650146",
+        latitudeDelta: "0.1942030042",
+        longitudeDelta: "0.1206780002"
       },
       {
         id: 8,
@@ -86,82 +86,82 @@ class RegionSeeder {
       {
         id: 9,
         name: "Hauts-de-France",
-        latitude: "50.278182",
-        longitude: "2.2230675",
-        latitudeDelta: "-0.497070000000001",
-        longitudeDelta: "0.711545"
+        latitude: "50.2781829834",
+        longitude: "2.2230675220",
+        latitudeDelta: "0.4970700145",
+        longitudeDelta: "0.7115449905"
       },
       {
         id: 10,
         name: "Île-de-France",
-        latitude: "48.842859",
-        longitude: "2.1150125",
-        latitudeDelta: "0.414055999999995",
-        longitudeDelta: "0.807841"
+        latitude: "48.8619918823",
+        longitude: "2.4195160866",
+        latitudeDelta: "0.0260049999",
+        longitudeDelta: "0.1903219968"
       },
       {
         id: 11,
         name: "Martinique",
-        latitude: "14.792889",
-        longitude: "-61.0234375",
-        latitudeDelta: "0.041708",
-        longitudeDelta: "0.312267000000006"
+        latitude: "14.6968536377",
+        longitude: "-60.9671363831",
+        latitudeDelta: "0.0810780004",
+        longitudeDelta: "0.0799150020"
       },
       {
         id: 12,
         name: "Mayotte",
-        latitude: "-12.715386",
-        longitude: "45.093109",
-        latitudeDelta: "0.137008",
-        longitudeDelta: "0.089244000000001"
+        latitude: "-12.8235807419",
+        longitude: "45.1628303528",
+        latitudeDelta: "0.0188689996",
+        longitudeDelta: "0.0431820005"
       },
       {
         id: 13,
         name: "Normandie",
-        latitude: "49.402837",
-        longitude: "0.724564",
-        latitudeDelta: "0.654788000000004",
-        longitudeDelta: "0.36329"
+        latitude: "49.4028358459",
+        longitude: "0.7245640159",
+        latitudeDelta: "0.6547880173",
+        longitudeDelta: "0.3632900119"
       },
       {
         id: 14,
         name: "Nouvelle-Aquitaine",
-        latitude: "44.5385645",
-        longitude: "-0.210337",
-        latitudeDelta: "2.326099",
-        longitudeDelta: "0.39016"
+        latitude: "44.6293907166",
+        longitude: "-0.0864780024",
+        latitudeDelta: "0.3852959871",
+        longitudeDelta: "0.1656540036"
       },
       {
         id: 15,
         name: "Occitanie",
-        latitude: "43.4657897949",
-        longitude: "2.0390141010",
-        latitudeDelta: "1.0640560389",
-        longitudeDelta: "0.9906520247"
+        latitude: "43.5997619629",
+        longitude: "2.0368914604",
+        latitudeDelta: "0.2175240070",
+        longitudeDelta: "0.1033729985"
       },
       {
         id: 16,
         name: "Pays de la Loire",
-        latitude: "47.70813",
-        longitude: "-0.903561",
-        latitudeDelta: "0.871029999999998",
-        longitudeDelta: "-0.31147"
+        latitude: "47.4516067505",
+        longitude: "-0.9597924948",
+        latitudeDelta: "0.2130690068",
+        longitudeDelta: "0.1834229976"
       },
       {
         id: 17,
         name: "Provence-Alpes-Côte d'Azur",
-        latitude: "43.7902215",
-        longitude: "6.232034",
-        latitudeDelta: "0.602398999999998",
-        longitudeDelta: "0.055148"
+        latitude: "43.7902221680",
+        longitude: "6.2320342064",
+        latitudeDelta: "0.6023989916",
+        longitudeDelta: "0.0551480018"
       },
       {
         id: 18,
         name: "Réunion",
-        latitude: "-21.1202585",
-        longitude: "55.546931",
-        latitudeDelta: "0.081160999999998",
-        longitudeDelta: "0.199078"
+        latitude: "-21.1419754028",
+        longitude: "55.5605049133",
+        latitudeDelta: "0.0005240000",
+        longitudeDelta: "0.0649499968"
       }
     ]
 
