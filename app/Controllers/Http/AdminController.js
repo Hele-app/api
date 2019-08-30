e110777dcf5bb7a12f7b2a09c8b94cd7ab2191e1
Dylan <dylan.coodien@epitech.eu>
2019-08-30 18:51:33 +0200

    CRUD User in admin

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -5,86 +5,81 @@ const { validateAll } = use('Validator')
 const { ValidationException } = use('@adonisjs/validator/src/Exceptions')
 
 class AdminController {
-    async getall({request, auth, response}) {
-        let pro = await User.all()
-        return response.json({pro})
+  async index({request, auth, response}) {
+    let users = await User.all()
+    return response.json({users})
+  }
+
+  async findUser({request, auth, response, params: {id} }) {
+    let user = await User.findOrFail(id)
+    return response.json({user})
+  }
+
+  async create({request, auth, response}) {
+    const validation = await validateAll(request.all(), {
+      phone: 'required|unique:users|regex:^0[6-7](\\d{2}){4}$',
+      username: 'required|unique:users',
+      birthyear: 'required',
+      region_id: 'required',
+      roles: 'required'
+    })
+
+    if (validation.fails()) {
+      throw new ValidationException(validation.messages(), 400)
     }
 
-    async getusers({request, auth, response}) {
-        const roles = request.input('roles')
-        const pro = await User.query().where("roles", roles).fetch()
-        return response.json({pro})
+    const password = (
+      Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+    ).substring(0, 10)
+
+    let user = new User()
+    this.fillUser(user, request)
+    user.password = password
+
+    await user.save()
+    // TODO: send SMS with password instead of sending it in the response.
+
+    return response.json({user})
+  }
+
+  async update({request, auth, response, params: {id} }) {
+    const validation = await validateAll(request.all(), {
+      phone: 'unique:users|regex:^0[6-7](\\d{2}){4}$',
+      username: 'unique:users',
+    })
+
+    if (validation.fails()) {
+      throw new ValidationException(validation.messages(), 400)
     }
 
-    async verifyid({request, auth, response}) {
-      const id = request.input('id')
-      const pro = await User.findOrFail(id)
-      return response.json({pro})
-    }
+    const user = await User.findOrFail(id)
+    this.fillUser(user, request)
+    await user.save()
+    // TODO: send SMS with password instead of sending it in the response.
 
-    async updateuser({request, auth, response}){
-      const id = request.input('id')
-      const params = JSON.parse(request.input('params'))
-      const validation = await validateAll(params, {
-        phone : 'unique:users',
-        email:'unique:users',
-        username: 'unique:users',
-      })
-      if (validation.fails()) {
-        throw new ValidationException(validation.messages(), 400)
-      }
-      const user = await User.findOrFail(id)
-      for(var param in params){
-        user[param] = params[param]
-      }
-      await user.save()
-      return response.json({'200' : "L'update a bien fonctionné !"})
-    }
+    return response.json({user})
+  }
 
-    async createadmin({request, auth, response}) {
-        const validation = await validateAll(request.all(), {
-            phone : 'required|unique:users',
-            email:'required|unique:users',
-            username: 'required|unique:users',
-            age: 'required|integer|above:10',
-            region: 'required',
-            city: 'required',
-            profession: 'required',
-            professional_phone: 'required',
-            roles : 'required'
-          })
-          if (validation.fails()) {
-            throw new ValidationException(validation.messages(), 400)
-          }
+  async delete({request, auth, response, params: {id} }) {
+    const user = await User.findOrFail(id)
+    await user.delete()
+    console.log(user)
+    return response.json({"user" : `L'utilisateur ${user.username} à bien été supprimé.`})
+  }
 
-          const password = (
-            Math.random().toString(36).substring(2, 15) +
-            Math.random().toString(36).substring(2, 15)
-          ).substring(0, 10)
-
-          let user = new User()
-          user.phone = request.input('phone')
-          user.username = request.input('username')
-          user.region = request.input('region')
-          user.email = request.input('email')
-          user.roles = request.input('roles')
-          user.city = request.input('city')
-          user.profession = request.input('profession')
-          user.phone_pro = request.input('professional_phone')
-          user.birthyear = new Date().getFullYear() - request.input('age')
-          user.password = password
-          await user.save()
-
-          // send the pass with a sms
-          return response.json({"c" : "bon"})
-    }
-    async deleteuser({request, auth, response}) {
-        const id = request.input('id_user')
-        console.log(request.all())
-        const user = await User.find(id)
-        await user.delete()
-        return response.json({"user" : "L'utilisateur à bien été supprimé."})
-    }
+  async fillUser(user, request) {
+    user.phone = request.input('phone')
+    user.email = request.input('email')
+    user.username = request.input('username')
+    user.birthyear = request.input('birthyear')
+    user.region_id = request.input('region_id')
+    user.city = request.input('city')
+    user.roles = request.input('roles')
+    user.profession = request.input('profession')
+    user.phone_pro = request.input('phone_pro')
+    user.active = request.input('active')
+  }
 }
 
 module.exports = AdminController
