a01176c872f39600d80f642161096f73afa47e0a
LeNiglo <lefrantguillaume@gmail.com>
2020-03-16 23:40:54 +0100

    password reset migration

---
+++ database/migrations/1584374445126_password_reset_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PasswordResetSchema extends Schema {
+  up () {
+    this.create('password_resets', (table) => {
+      table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('code').notNullable().unique().index()
+      table.boolean('is_used').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('password_resets')
+  }
+}
+
+module.exports = PasswordResetSchema
