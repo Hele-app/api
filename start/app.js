96dc2cb06a5f39bfd1916ba4e8b35df8964d99bf
LeNiglo <lefrantguillaume@gmail.com>
2020-03-27 18:40:26 +0100

    lint

--- start/app.js
+++ start/app.js
@@ -17,8 +17,7 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/validator/providers/ValidatorProvider',
-
-  'adonis-sentry/providers/Sentry',
+  'adonis-sentry/providers/Sentry'
 ]
 
 /*
