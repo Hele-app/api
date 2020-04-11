b42b7e53220ee95d1ffca70e838176e99b1d9ec3
Dylan <dylan.coodien@epitech.eu>
2020-04-11 18:02:29 +0200

    Update pro creation validation

--- app/Validators/User/Pro/Store.js
+++ app/Validators/User/Pro/Store.js
@@ -7,9 +7,9 @@ class Store {
   get rules() {
     return {
       phone: 'required|regex:^0[6-7](\\d{2}){4}$|unique:users,phone',
-      username: 'required|regex:^[a-z]+[a-z0-9]+$',
+      username: 'required|regex:^[a-z]+[a-z0-9]+$|unique:users,username',
       birthyear: 'required|integer|above:1800',
-      email: 'required|email',
+      email: 'required|email|unique:users,email',
       role: 'in:MODERATOR,PROFESSIONAL,ADMIN',
       phone_pro: 'regex:^0[6-7](\\d{2}){4}$'
     }
@@ -22,12 +22,14 @@ class Store {
       'phone.unique': 'E_PHONE_NOT_UNIQUE',
       'username.required': 'E_USERNAME_REQUIRED',
       'username.regex': 'E_USERNAME_WRONG_FORMAT',
+      'username.unique': 'E_USERNAME_NOT_UNIQUE',
       'birthyear.required': 'E_BIRTHYEAR_REQUIRED',
       'birthyear.integer': 'E_BIRTHYEAR_WRONG_FORMAT',
       'birthyear.above': 'E_BIRTHYEAR_WRONG_FORMAT',
       'email.required': 'E_EMAIL_REQUIRED',
       'email.email': 'E_EMAIL_WRONG_FORMAT',
-      'role.in': 'E_ROLE_WRONG_FORMAT',
+      'email.unique': 'E_EMAIL_NOT_UNIQUE',
+      'role.in': 'E_ROLE_NOT_PRO',
       'phone_pro.regex': 'E_PHONE_PRO_WRONG_FORMAT'
     }
   }
