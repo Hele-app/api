6c1dfebdef9aa609fd5a97c66182ccffeddfa71a
e-gloo <dylan.coodien@epitech.eu>
2020-06-20 22:53:02 +0200

    Add on delete to relationship

--- config/database/migrations/20200620205928_establishment.js
+++ config/database/migrations/20200620205928_establishment.js
@@ -7,7 +7,7 @@ exports.up = function(knex) {
       table.string('name', 255).notNullable()
       table.string('code', 10).notNullable().unique()
       table.integer('region_id').notNullable()
-        .unsigned().references('id').inTable('regions')
+        .unsigned().references('id').inTable('regions').onDelete('NO ACTION')
       table.timestamps()
     })
 }
