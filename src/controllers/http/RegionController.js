3b8248350be20555be5c3762d6cf69e2e0c82d85
LeNiglo <lefrantguillaume@gmail.com>
2020-06-22 00:46:40 +0200

    removed comment and unused param

--- src/controllers/http/RegionController.js
+++ src/controllers/http/RegionController.js
@@ -3,14 +3,7 @@
 import db from '../../../config/database'
 
 export default class RegionController {
-  /**
-   * Returns all regions with no search nor pagination
-   * GET regions/all
-   *
-   * @param {object} ctx
-   * @param {Response} ctx.response
-   */
-  static async all(req, res, next) {
+  static async all(req, res) {
     const regions = await db('regions').select('*')
     return res.json(regions)
   }
