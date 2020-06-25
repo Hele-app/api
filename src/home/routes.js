092ca29b9201aba5dc22ae6075081464393881bb
e-gloo <dylan.coodien@epitech.eu>
2020-06-25 19:58:52 +0200

    Update route access

--- src/home/routes.js
+++ src/home/routes.js
@@ -1,10 +1,11 @@
 'use strict'
 
 import { Router } from 'express'
+import { loggedIn, IsRole } from '../commons/middlewares'
 import HttpController from './HttpController'
 
 const router = Router()
 
-router.get('/', HttpController.index)
+router.get('/', loggedIn, new IsRole('YOUNG'), HttpController.index)
 
 export default router
