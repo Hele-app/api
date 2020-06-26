142c82faf597d217e3e15b390337d19b2d7382a4
LeNiglo <lefrantguillaume@gmail.com>
2020-06-26 12:14:02 +0200

    map_pois route, controller, validators and DAO

---
+++ src/map/routes.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { Router } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '../commons/helpers'
+import { store } from './validators'
+import { loggedIn } from '../commons/middlewares'
+import { IsRole } from '../commons/middlewares/auth'
+import HttpController from './HttpController'
+
+const storeSchema = checkSchema(store, ['body'])
+const router = Router()
+
+router.use(loggedIn)
+router.get('/region/:region', HttpController.getRegion)
+router.get('/', new IsRole('ADMIN'), HttpController.index)
+router.post('/', new IsRole('ADMIN'), validate(storeSchema), HttpController.store)
+router.put('/:id', new IsRole('ADMIN'), validate(storeSchema), HttpController.update)
+router.delete('/:id', new IsRole('ADMIN'), HttpController.destroy)
+
+export default router
