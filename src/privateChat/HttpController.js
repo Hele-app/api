502b1fc61700d2388bfb77cebd6a2802f18f10e0
e-gloo <dylan.coodien@epitech.eu>
2020-07-09 19:13:01 +0200

    Update last messages

--- src/privateChat/HttpController.js
+++ src/privateChat/HttpController.js
@@ -1,7 +1,11 @@
 'use strict'
 
+import { Message } from '../commons/models'
+
 export default class ChatController {
   static async lastMessages(req, res) {
-    return res.json({ data: {} })
+    const messages = await Message.query({ where: { chat_id: req.params.id } })
+      .orderBy('created_at', 'DESC').fetchPage({ page: req.query.p || 1 })
+    return res.json({ data: messages })
   }
 }
